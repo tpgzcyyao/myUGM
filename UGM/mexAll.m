f1a3ac646de02e7b90c351bdec67378abe01848c
Kelin LU <landcolin@gmail.com>
2013-07-04 17:20:39 +0800

    mex UGM_makeCRFmapsC.c
    mex UGM_makeCRFmapsC.c

--- UGM/mexAll.m
+++ UGM/mexAll.m
@@ -23,3 +23,5 @@ mex -Imex -outdir compiled mex/UGM_LogConfigurationPotentialC.c
 mex -Imex -outdir compiled mex/UGM_Decode_AlphaExpansionC.c
 mex -Imex -outdir compiled mex/UGM_Decode_AlphaExpansionBetaShrinkC.c
 mex -Imex -outdir compiled mex/UGM_CRF_NLLC.c
+
+mex -Imex -outdir compiled mex/UGM_makeCRFmapsC.c
