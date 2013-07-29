077540b2daf7cc81b8fbbe001bb3601cd3176247
Kelin LU <landcolin@gmail.com>
2013-07-29 10:34:17 +0800

    add graphViz4Matlab

---
+++ UGM/graphViz4Matlab/addtosystempathGV.m
@@ -0,0 +1,13 @@
+function addtosystempathGV(p)
+% Add p to the system path. If p is already on the path, this does nothing.
+% Note, this change only persists for the duration of the Matlab session.
+% Do not include path delimiters like ; or : .
+% Example:
+% addtosystempathGV('C:\Users\matt\bin');
+% addtosystempathGV('D:\Program Files\Graphviz2.30\bin');
+
+if isempty(getenv('PATH')) || all(cellfun(@(c)isempty(c),strfind(tokenizeGV(getenv('PATH'),pathsep()),p)));
+    setenv('PATH', [getenv('PATH'), pathsep(),p]);
+end
+
+end
